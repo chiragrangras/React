f31ce6751c1758c01a8f730c445534968bbaa3a8
Chirag Rangras <chiragrangras012@gmail.com>
2024-01-31 21:12:39 +0530

    new file

--- React/test1/src/App.js
+++ React/test1/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import FetchApiData from './FetchApiData.jsx';
 import Compo1, { Compo2 } from './component.js';
 import Test2 from './component1.jsx';
+import Class2, { Class1, Fun1, Fun2 } from './component2.jsx';
 
 
 function App(){
@@ -12,6 +13,11 @@ function App(){
       <Compo1/>
       <Compo2/>
       <Test2/>
+      <Fun1/>
+      <Fun2 x="12" y="34"/>
+
+      <Class1/>
+      <Class2/>
       
     </div>
   )
