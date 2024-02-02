075baf178976376c8f916dc16520ebcc980c54e9
Chirag Rangras <chiragrangras012@gmail.com>
2024-02-02 17:01:09 +0530

    new file

---
+++ React/Practical/test2/src/App.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  function GetAnimateion() {
+    document.getElementById("d1").className = "box";
+  }
+
+  let n1 = document.getElementById('n1');
+  let n2 = document.getElementById('n2');
+  let result = document.getElementById('res');
+  let op = document.getElementById('oprator');
+
+  function Cal()
+  {
+    switch(op)
+    {
+      case "+": result.innerHTML = n1+n2;
+        break;
+
+        case "-": result.innerHTML = n1-n2;
+        break;
+
+        case "*": result.innerHTML = n1*n2;
+        break;
+
+        case "/": result.innerHTML = n1/n2;
+        break;
+
+        case "%": result.innerHTML = n1%n2;
+        break;
+
+        default: result.innerHTML = "Value not defined..!";
+    }
+  }
+
+  return (
+    <div className="App">
+
+      num1 : <input type="text"id="n1"></input><br/>
+      num2 : <input type="text" id="n2"></input><br/>
+      <b id="res"></b>
+      <input type="text" id="oprator"></input>
+      <button onClick={Cal}>Calculate</button>
+
+      <div id="d1"></div>
+      <button onClick={GetAnimateion}>Animate</button>
+    </div>
+  )
+}
+
+export default App;
