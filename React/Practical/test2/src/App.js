b2deb65ced86fc51521120d2fd8900f90cf4fcb3
Chirag Rangras <chiragrangras012@gmail.com>
2024-02-03 11:53:18 +0530

    new file

--- React/Practical/test2/src/App.js
+++ React/Practical/test2/src/App.js
@@ -6,47 +6,50 @@ function App() {
     document.getElementById("d1").className = "box";
   }
 
-  let n1 = document.getElementById('n1');
-  let n2 = document.getElementById('n2');
-  let result = document.getElementById('res');
-  let op = document.getElementById('oprator');
-
-  function Cal()
-  {
-    switch(op)
-    {
-      case "+": result.innerHTML = n1+n2;
+  function Cal() {
+    let n1 = document.getElementById("n1");
+    let n2 = document.getElementById("n2");
+    let result = document.getElementById("res");
+    let op = document.getElementById("operator");
+    switch (op) {
+      case "+":
+        result.innerHTML = n1 + n2;
         break;
 
-        case "-": result.innerHTML = n1-n2;
+      case "-":
+        result.innerHTML = n1 - n2;
         break;
 
-        case "*": result.innerHTML = n1*n2;
+      case "*":
+        result.innerHTML = n1 * n2;
         break;
 
-        case "/": result.innerHTML = n1/n2;
+      case "/":
+        result.innerHTML = n1 / n2;
         break;
 
-        case "%": result.innerHTML = n1%n2;
+      case "%":
+        result.innerHTML = n1 % n2;
         break;
 
-        default: result.innerHTML = "Value not defined..!";
+      default:
+        result.innerHTML = "Value is a not defined..!";
     }
   }
 
   return (
     <div className="App">
-
-      num1 : <input type="text"id="n1"></input><br/>
-      num2 : <input type="text" id="n2"></input><br/>
+      num1 : <input type="text" id="n1"></input>
+      <br />
+      num2 : <input type="text" id="n2"></input>
+      <br />
       <b id="res"></b>
-      <input type="text" id="oprator"></input>
+      <input type="text" id="operator"></input>
       <button onClick={Cal}>Calculate</button>
-
       <div id="d1"></div>
       <button onClick={GetAnimateion}>Animate</button>
     </div>
-  )
+  );
 }
 
 export default App;
