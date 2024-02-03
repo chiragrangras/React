9b30c04e337772d55e88871cfe22ade1568857d8
Chirag Rangras <chiragrangras012@gmail.com>
2024-02-03 21:07:11 +0530

    new file

---
+++ React/test2/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TestState from './ClassState';
+
+function App() {
+  return (
+    <div className="App">
+      <TestState/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/test2/src/ClassState.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class TestState extends React.Component{
+    render()
+    {
+        return(
+            <>
+            <h1>Class</h1>
+            </>
+        )
+    }
+}
+
+export default TestState;
