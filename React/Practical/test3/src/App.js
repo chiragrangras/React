7653a92eb11d5d579790ea309edf5c49f685343b
Chirag Rangras <chiragrangras012@gmail.com>
2024-02-05 11:39:04 +0530

    new file

--- React/Practical/test3/src/App.js
+++ React/Practical/test3/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 // import TestState from './ClassState';
 import StateEx1 from './StateEx1';
 import StateEx2 from './StateEx2';
+import StateEx3 from './StateEx3';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       {/* <TestState/> */}
       <StateEx1/>
       <StateEx2/>
+      <StateEx3/>
     </div>
   );
 }
