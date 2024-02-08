3695b5098af6da06ce8e1f2e1eedecc1e685d179
Chirag Rangras <chiragrangras012@gmail.com>
2024-02-08 17:35:23 +0530

    new file

--- React/arraystate/src/App.js
+++ React/arraystate/src/App.js
@@ -1,9 +1,22 @@
+import AddTitle from './AddTitle';
 import './App.css';
 
+let intialTodos = [
+  {title:'Apple'},
+  {title:'Mango'},
+  {title:'Orange'}
+];
+
 function App() {
+
+  let handleAdd = (title) => {
+    
+    console.log(title)
+  }
+  
   return (
     <div className="App">
-      
+      <AddTitle onAdd={handleAdd}/>
     </div>
   );
 }
