28f32920bcc1ce7df2b8d3d96a86672f8772f1a2
Chirag Rangras <chiragrangras012@gmail.com>
2024-02-09 16:02:56 +0530

    new file

--- React/arraystate/src/TaskList.js
+++ React/arraystate/src/TaskList.js
@@ -1,16 +1,58 @@
-import React from "react"
+import React, { useState } from "react"
 
-function TaskList(){
+function TaskList({confusion,onchangeTodo,ondeleteTodo}){
     return(
-        <div>
 
+        <div >
+            {confusion.map((v)=>{
+                return(<>
+                    <li>
+                        {/* {v.name} */}
+
+                        <Task
+                            todo={v}
+                        />
+                    </li>
+                </>)
+            })}
         </div>
     )
 }
 
-function Task()
+function Task({todo,onchange,ondelete})
 {
-    
+    // console.log(todo)
+    let [edit,setEdit] = useState(false);
+    let content;
+
+    if(edit)
+    {
+        content = 
+        <>
+        <input type="text" value={todo.name} onChange={(e)=>{
+            onchange({
+                name:e.target.value
+            })
+            }}/>
+            <button>Save</button>
+        </>
+    }
+    else
+    {
+        content =
+        <>
+        {todo.name}
+        <button onClick={()=>{setEdit(true)}}>Edit</button>
+        </>
+    }
+
+    return(
+        <>
+        
+        {content}
+        <button onClick={()=>{ondelete(todo.id)}}>Delete</button>
+        </>
+    )
 }
 
 export default TaskList
