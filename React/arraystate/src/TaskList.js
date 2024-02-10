00f6f77fb2e88f4e8174513fe8b1a8831f7cbead
Chirag Rangras <chiragrangras012@gmail.com>
2024-02-10 23:13:37 +0530

    new file

--- React/arraystate/src/TaskList.js
+++ React/arraystate/src/TaskList.js
@@ -1,58 +1,72 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 
-function TaskList({confusion,onchangeTodo,ondeleteTodo}){
-    return(
+function TaskList({ confusion, onchangeTodo, ondeleteTodo }) {
+  return (
+    <div>
+        {confusion.map((v)=>(
+            <div key={v.id}>
+                <li>
+                    {/* {v.name} */}
 
-        <div >
-            {confusion.map((v)=>{
-                return(<>
-                    <li>
-                        {/* {v.name} */}
+                    <Task
 
-                        <Task
-                            todo={v}
-                        />
-                    </li>
-                </>)
-            })}
-        </div>
-    )
+                    todo={v}
+                    onchange={onchangeTodo}
+                    />
+                </li>
+            </div>
+        ))}
+    </div>
+  )
 }
 
-function Task({todo,onchange,ondelete})
-{
-    // console.log(todo)
-    let [edit,setEdit] = useState(false);
-    let content;
+function Task({ todo, onchange, ondelete }) {
+//   console.log(todo)
+  let [edit, setEdit] = useState(false);
+  let content;
 
-    if(edit)
-    {
-        content = 
-        <>
-        <input type="text" value={todo.name} onChange={(e)=>{
+  if (edit) {
+    content = (
+      <>
+        <input
+          type="text"
+          value={todo.name}
+          onChange={(e) => {
             onchange({
-                name:e.target.value
-            })
-            }}/>
-            <button>Save</button>
-        </>
-    }
-    else
-    {
-        content =
-        <>
+              name: e.target.value,
+            });
+          }}
+        />
+        <button>Save</button>
+      </>
+    );
+  } else {
+    content = (
+      <>
         {todo.name}
-        <button onClick={()=>{setEdit(true)}}>Edit</button>
-        </>
-    }
+        <button
+          onClick={() => {
+            setEdit(true);
+          }}
+        >
+          Edit
+        </button>
+      </>
+    );
+  }
 
-    return(
-        <>
-        
-        {content}
-        <button onClick={()=>{ondelete(todo.id)}}>Delete</button>
-        </>
-    )
+  return (
+    <>
+      {content}
+      <button
+        onClick={() => {
+          ondelete(todo.id);
+        }}
+      >
+        Delete
+      </button>
+    </>
+  );
 }
 
-export default TaskList
+export default TaskList;
