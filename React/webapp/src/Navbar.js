206361926e25626ebd9cc65c635857d292937d0d
Chirag Rangras <chiragrangras012@gmail.com>
2024-02-14 20:30:33 +0530

    new file

---
+++ React/webapp/src/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function Navbar(){
+    return(
+        <div>
+            
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="">About Us</a></li>
+                    <li><a href="">Contact Us</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+export default Navbar
