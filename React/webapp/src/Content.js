9ce237578145618225a4b1e88198210593662df3
Chirag Rangras <chiragrangras012@gmail.com>
2024-02-14 20:29:28 +0530

    new file

---
+++ React/webapp/src/Content.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+function Content(){
+    return(
+        <div>
+            <section>
+                <div className="cards">
+                    <div className="card">Card 1</div>
+                    <div className="card">Card 2</div>
+                    <div className="card">Card 3</div>
+                    <div className="card">Card 4</div>
+
+                    <div className="card">Card 5</div>
+                    <div className="card">Card 6</div>
+                    <div className="card">Card 7</div>
+                    <div className="card">Card 8</div>
+                </div>
+            </section>
+        </div>
+    )
+}
+export default Content
