6fa806b4231adb10d2e4097a7f95d5c5760a3434
Chirag Rangras <chiragrangras012@gmail.com>
2024-02-14 20:28:14 +0530

    new file

---
+++ React/webapp/src/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Header(){
+    return(
+        <div>
+            
+            <header>
+                <div className="log">Logo</div>
+                <div className="cname">Company Name</div>
+            </header>
+        </div>
+    )
+}
+export default Header
