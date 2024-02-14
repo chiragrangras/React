f81290a6cd16f4ae8af0df45b39004d7666f4a0f
Chirag Rangras <chiragrangras012@gmail.com>
2024-02-14 20:30:05 +0530

    new file

---
+++ React/webapp/src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Footer(){
+    return(
+        <div>
+            <footer>
+                <h1>Footer title</h1>
+                <p>Footer Body</p>
+            </footer>
+        </div>
+    )
+}
+export default Footer
