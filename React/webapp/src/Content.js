84bfa0a19ddcd3b5fde4a81daad42133cf2c5229
Chirag Rangras <chiragrangras012@gmail.com>
2024-02-14 21:16:10 +0530

    new file

--- React/webapp/src/Content.js
+++ React/webapp/src/Content.js
@@ -1,16 +1,21 @@
 import React from "react"
+import car from './Car.jpg'
 
 function Content(){
     return(
         <div>
             <section>
                 <div className="cards">
-                    <div className="card">Card 1</div>
+                    <div className="card">
+                        <img src="https://assets.mspimages.in/gear/wp-content/uploads/2023/01/Bugati.png" height={100} width={100}/>
+                    </div>
                     <div className="card">Card 2</div>
                     <div className="card">Card 3</div>
                     <div className="card">Card 4</div>
 
-                    <div className="card">Card 5</div>
+                    <div className="card">
+                        <img src={car} height={100} width={100}></img>
+                    </div>
                     <div className="card">Card 6</div>
                     <div className="card">Card 7</div>
                     <div className="card">Card 8</div>
