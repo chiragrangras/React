5e2d7be21c04cd15955ebfa1aeb3332202bbbb4f
Chirag Rangras <chiragrangras012@gmail.com>
2024-02-15 17:32:31 +0530

    new file

--- React/webapp/src/Content.js
+++ React/webapp/src/Content.js
@@ -7,14 +7,16 @@ function Content(){
             <section>
                 <div className="cards">
                     <div className="card">
-                        <img src="https://assets.mspimages.in/gear/wp-content/uploads/2023/01/Bugati.png" height={100} width={100}/>
+                        <img src="https://assets.mspimages.in/gear/wp-content/uploads/2023/01/Bugati.png" alt="" height={100} width={100}/>
                     </div>
                     <div className="card">Card 2</div>
-                    <div className="card">Card 3</div>
+                    <div className="card">
+                        <img src="Img/Car.jpg" alt="" height={100} width={100}></img>
+                    </div>
                     <div className="card">Card 4</div>
 
                     <div className="card">
-                        <img src={car} height={100} width={100}></img>
+                        <img src={car} alt="" height={100} width={100}></img>
                     </div>
                     <div className="card">Card 6</div>
                     <div className="card">Card 7</div>
