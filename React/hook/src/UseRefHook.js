cce0d630640f770164242367b022b5c18cdf03a5
Chirag Rangras <chiragrangras012@gmail.com>
2024-02-16 19:24:11 +0530

    new file

--- React/hook/src/UseRefHook.js
+++ React/hook/src/UseRefHook.js
@@ -1,14 +1,38 @@
-import React from "react"
+import React, { useRef } from "react"
 
 function UseRefHook(){
+
+    let h2Ref = useRef();
+    let divRef = useRef();
+
+    let testDivs = () =>{
+        // let divs = document.querySelectorAll('.x')
+        // console.log(divs)
+
+        console.log(h2Ref)
+        console.log(h2Ref.current)
+        console.log(divRef)
+
+        h2Ref.current.innerHTML = 'Wow Chirag';
+        h2Ref.current.style.color = 'blue';
+
+        divRef.current.style.background = 'coral'
+    }
     return(
         <div>
+
+            <h2 ref={h2Ref}>Hello Ref Hook</h2>
+
+            <div className="p" ref={divRef}>
 
             <div className="x">div 1</div>
             <div className="x">div 2</div>
             <div className="x">div 3</div>
             <div className="x">div 4</div>
+
+            </div>
         
+            <button onClick={testDivs}>Get Divs</button>
         </div>
     )
 }
