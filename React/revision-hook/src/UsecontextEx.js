0051869e3ed220a4a4de40d6d02d1d317ee38f6f
Chirag Rangras <chiragrangras012@gmail.com>
2024-02-17 21:32:08 +0530

    new file

---
+++ React/revision-hook/src/UsecontextEx.js
@@ -0,0 +1,17 @@
+import About from "./About";
+import Blogs from "./Blogs";
+import Home from "./Home";
+import Services from "./Services";
+
+function UsecontextEx(){
+    return(
+        <div>
+            <Home/>
+            <About/>
+            <Services/>
+            <Blogs/>
+        </div>
+    )
+}
+
+export default UsecontextEx
