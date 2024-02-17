6803c8ab64d36276cbcbba46e85d17e57ece5017
Chirag Rangras <chiragrangras012@gmail.com>
2024-02-17 22:55:50 +0530

    new file

--- React/revision-hook/src/UsecontextEx.js
+++ React/revision-hook/src/UsecontextEx.js
@@ -2,10 +2,15 @@ import About from "./About";
 import Blogs from "./Blogs";
 import Home from "./Home";
 import Services from "./Services";
+import { data } from "./App";
+import { useContext } from "react";
 
 function UsecontextEx(){
+
+    let val = useContext(data)
     return(
         <div>
+            {val}
             <Home/>
             <About/>
             <Services/>
