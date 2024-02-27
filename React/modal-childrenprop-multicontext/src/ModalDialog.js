425337762eb73177e687b67c543de78d30417139
Chirag Rangras <chiragrangras012@gmail.com>
2024-02-27 11:05:43 +0530

    new file

--- React/modal-childrenprop-multicontext/src/ModalDialog.js
+++ React/modal-childrenprop-multicontext/src/ModalDialog.js
@@ -2,23 +2,7 @@ import React, { useEffect, useRef } from "react";
 
 function ModalDialog({children,x}) {
     
-    let ref = useRef();
-
-    useEffect(()=>{
-        let dialog = ref.current
-        console.log(dialog)
-        console.log(x)
-
-        if(x)
-        {
-            
-        }
-        else
-        {
-            dialog.showModal();
-        }
-    })
-
+    
   return (
 
       <dialog ref={ref}>{children}</dialog>
