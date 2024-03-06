e5850422ee5c3d8c17450c5a05ebe7269ad4a71c
Chirag Rangras <chiragrangras012@gmail.com>
2024-03-06 20:18:53 +0530

    new file

--- React/usereducer-todolist/src/Todos.js
+++ React/usereducer-todolist/src/Todos.js
@@ -4,7 +4,13 @@ let initialState = [];
 function reducer(state, action) {
   switch (action.type) {
     case "Add_Task":
-
+    return [
+      ...state,
+      {
+        id: state.lenght +1,
+        name : action.name
+      }
+    ]
     default:
       return state;
   }
@@ -14,9 +20,13 @@ let Todos = () => {
 
   return (
     <>
-      <h4>Todo List {todos.length}</h4>
-      Enter New Task:
-      <input type="text" onChange={(e)=>{dispatch({type: 'Add_Task',name:e.target.value})}}/>
+      {/* <h4>Todo List {todos.length}</h4> */}
+      <label>Enter New Task: &nbsp;</label>
+      <input type="text" 
+        onChange={(e)=>{
+          dispatch({type: 'Add_Task', name:e.target.value}
+          )}}/>
+          {todos.map(todo => <li key={todo.id}>{todo.name}</li>)}
     </>
   );
 };
