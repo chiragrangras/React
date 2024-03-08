e9b37b4b22bc48898a296b35d06cb60ab05f34cf
Chirag Rangras <chiragrangras012@gmail.com>
2024-03-08 16:23:12 +0530

    new file

---
+++ React/booking/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Header from "./Header";
+import Nav from './Nav';
+import Search from "./Search";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Nav/>
+      <Search/>
+    </div>
+  );
+}
+
+export default App;
