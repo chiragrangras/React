3aad09e7f89d29363999584b0168edc2e2258372
Chirag Rangras <chiragrangras012@gmail.com>
2024-03-08 22:40:46 +0530

    new file

---
+++ React/booking/src/SearchResult.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Nav from "./Nav";
+
+function Search() {
+  return (
+    <div>
+      <Header/>
+      <Nav/>
+      <div>Search results will appear here.</div>
+      
+    </div>
+  );
+}
+export default Search;
