75792e93a85d48c64bb460c618cc5f87254c63e7
Chirag Rangras <chiragrangras012@gmail.com>
2024-03-10 19:06:55 +0530

    new file

--- React/booking/src/SearchResult.js
+++ React/booking/src/SearchResult.js
@@ -5,10 +5,18 @@ import Nav from "./Nav";
 function Search() {
   return (
     <div>
-      <Header/>
-      <Nav/>
-      <div>Search results will appear here.</div>
-      
+      <Header />
+      <Nav />
+      <div className="d-flex banner-img">
+        <div className="ml-5">
+          <h1 className="mt-5">
+            <span className="d-block banner-h-font">A dream stay</span>
+            <span className="d-block banner-h-font">for a bucket list trip</span>
+          </h1>
+          <p>Make it a trip to remember in a holiday home</p>
+          <a href="#" className="discover-btn">Discover holiday rentals</a>
+        </div>
+      </div>
     </div>
   );
 }
