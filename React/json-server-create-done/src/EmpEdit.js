dadb94c616e3eae40011ff3518fe5fe8ffa932ea
Chirag Rangras <chiragrangras012@gmail.com>
2024-03-15 22:54:11 +0530

    new file

--- React/json-server-create-done/src/EmpEdit.js
+++ React/json-server-create-done/src/EmpEdit.js
@@ -1,8 +1,25 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 function EditEmp() {
   let navigate = useNavigate();
+
+  let [id1,setId1] = useState('');
+  let [name,setName] = useState('');
+  let [salary,setSalary] = useState('');
+
+  let {eid} = useParams();
+  // console.log(eid)
+
+  useEffect(()=>{
+    fetch('http://localhost:4000/employees/'+eid)
+    .then((res)=>{ return res.json()})
+    .then((data)=>{
+      setId1(data.id)
+      setName(data.name)
+      setSalary(data.salary)
+    })
+  },[])
 
   let handleSubmit = (e) => {};
 
@@ -14,10 +31,20 @@ function EditEmp() {
         </div>
         <form onSubmit={handleSubmit}>
           <div className="form-group">
+          <div className="mb-3">
+              <label className="form-label">ID</label>
+              <input
+                type="text"
+                readOnly
+                value={id1}
+                className="form-control"
+              />
+            </div>
+
             <div className="mb-3">
               <label className="form-label">Name</label>
               <input
-                type="text"                
+                type="text"          
                 className="form-control"
               />
             </div>
