9d40595b3298d6a0fa3b43007bdad1b47d0d9481
Chirag Rangras <chiragrangras012@gmail.com>
2024-03-18 22:37:31 +0530

    modified file

--- React/json-server-create-done/src/Home.js
+++ React/json-server-create-done/src/Home.js
@@ -3,9 +3,8 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import { Link, useNavigate } from "react-router-dom";
 
 function Home() {
-
   let [details, setDetails] = useState();
-  
+
   let navigate = useNavigate();
 
   useEffect(() => {
@@ -19,16 +18,41 @@ function Home() {
       });
   }, []);
 
-  let handleView = (id)=>{
-    console.log(id)
+  let handleView = (id) => {
+    console.log(id);
     // navigate(`/viewemp/${id}`)
-    navigate('/viewemp/'+ id)
-  }
+    navigate("/viewemp/" + id);
+  };
 
-  let handleEdit = (id)=>{
-    console.log(id)
-    navigate('/editemp/'+ id)
-  }
+  let handleEdit = (id) => {
+    console.log(id);
+    navigate("/editemp/" + id);
+  };
+  const [confirmationId, setConfirmationId] = useState("");
+
+  let handleDelete = (id) => {
+    setConfirmationId(id);
+  };
+
+  let handleConfirmDelete = (id) => {
+    fetch("http://localhost:4000/employees/" + id, {
+      method: "DELETE",
+      headers: { "content-type": "application/json" },
+    }).then((res) => {
+      fetch("http://localhost:4000/employees")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setDetails(data);
+        });
+    });
+    setConfirmationId("");
+  };
+
+  const handleCancelDelete = () => {
+    setConfirmationId("");
+  };
 
   return (
     <div>
@@ -37,7 +61,9 @@ function Home() {
           <h2>Employee Data</h2>
         </div>
         <div>
-          <Link to='/addemp' className="my-3 btn btn-primary">Add Employee</Link>
+          <Link to="/addemp" className="my-3 btn btn-primary">
+            Add Employee
+          </Link>
         </div>
         <div className="container">
           <div className="row">
@@ -62,9 +88,37 @@ function Home() {
                         <td>{emp.email}</td>
                         <td>{emp.salary}</td>
                         <td>
-                          <button onClick={()=>{handleView(emp.id)}} className="btn bg-info">View</button>
-                          <button onClick={()=>{handleEdit(`${emp.id}`)}} className="btn bg-success mx-3">Edit</button>
-                          <button className="btn bg-danger">Delete</button>
+                          <button
+                            onClick={() => {
+                              handleView(emp.id);
+                            }}
+                            className="btn bg-info"
+                          >
+                            View
+                          </button>
+                          <button
+                            onClick={() => {
+                              handleEdit(`${emp.id}`);
+                            }}
+                            className="btn bg-success mx-3"
+                          >
+                            Edit
+                          </button>
+                          <button
+                            onClick={() => {
+                              handleDelete(`${emp.id}`);
+                            }}
+                            className="btn bg-danger"
+                          >
+                            Delete
+                          </button>
+                          {confirmationId === emp.id &&(
+                            <div className="confirmation-dialog">
+                              <p>Are you sure you want to delete?</p>
+                              <button onClick={()=>handleConfirmDelete(emp.id)}>Yes</button>
+                              <button onClick={handleCancelDelete}>No</button>
+                            </div>
+                          )}
                         </td>
                       </tr>
                     ))}
