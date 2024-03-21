51f6cdd921a9dd50c9c8c7d5f720bbe94af27a76
Chirag Rangras <chiragrangras012@gmail.com>
2024-03-21 17:54:11 +0530

    new file

--- React/json-server/src/Home.js
+++ React/json-server/src/Home.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 function Home() {
+  let [details, setDetails] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:4000/employees")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        // console.log(data)
+        setDetails(data);
+      });
+  }, []);
+
   return (
     <div>
       <div className="container">
         <div>
           <h2>Employee Data</h2>
+        </div>
+
+        <div>
+          <button className="btn btn-primary my-3">Add Employee</button>
         </div>
 
         <div className="contanier">
@@ -23,13 +40,21 @@ function Home() {
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td>Id :</td>
-                    <td>Name :</td>
-                    <td>Email :</td>
-                    <td>Salary :</td>
-                    <td>Action :</td>
-                  </tr>
+                  {/* {console.log(details)} */}
+                  {details &&
+                    details.map((emp) => (
+                      <tr>
+                        <td>{emp.id}</td>
+                        <td>{emp.name}</td>
+                        <td>{emp.email}</td>
+                        <td>{emp.salary}</td>
+                        <td>
+                          <button className="btn btn-info">View</button>
+                          <button className="btn btn-success mx-3">Edit</button>
+                          <button className="btn btn-danger">Delete</button>
+                        </td>
+                      </tr>
+                    ))}
                 </tbody>
               </table>
             </div>
