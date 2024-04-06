3d763aaafb6a9aa2cc20fed451f59258e02ad00f
Chirag Rangras <chiragrangras012@gmail.com>
2024-04-06 12:36:57 +0530

    new file

---
+++ React/custom-hooks/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MouseMoveCircle from './MouseMoveCircle';
+
+function App() {
+  return (
+    <div className="App">
+      <MouseMoveCircle/>
+    </div>
+  );
+}
+
+export default App;
