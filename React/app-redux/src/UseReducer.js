148e4761128a3565310ea872a7aafc01db9d02d5
Chirag Rangras <chiragrangras012@gmail.com>
2024-04-11 18:48:18 +0530

    modified file

--- React/app-redux/src/UseReducer.js
+++ React/app-redux/src/UseReducer.js
@@ -7,13 +7,21 @@ export let userSlice = createSlice({
     reducers:{
 
         addUser: (state,action)=>{
-            console.log(state)
+            // console.log(state)
+            state.push(action.payload)
             // console.log(action.payload)
         },
 
-        // editUser: (state,action)=>{
-            
-        // },
+        editUser: (state,action)=>{
+            state.push(action.payload)
+
+            let {id,name,email,phone} = action.payload
+            // console.log(id,name,email,phone)
+
+            state.filter((user)=>{
+                return user.id == id
+            })
+        },
 
         // deleteUser: (state,action)=>{
             
@@ -22,5 +30,5 @@ export let userSlice = createSlice({
     }
 })
 
-export const {addUser} = userSlice.actions
+export const {addUser ,editUser } = userSlice.actions
 export default userSlice.reducer;
