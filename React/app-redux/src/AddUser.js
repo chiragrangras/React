874f8c7a9462ac9c3e3b63c7ed9232fbc0b7d4f6
Chirag Rangras <chiragrangras012@gmail.com>
2024-04-11 18:46:26 +0530

    modified file

--- React/app-redux/src/AddUser.js
+++ React/app-redux/src/AddUser.js
@@ -1,22 +1,35 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addUser } from "./UseReducer";
-
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from "react-redux";
 
 function AddUser() {
+
+  let users = useSelector((state) => state.user);
+  // console.log(users)
+
   let [name, setName] = useState("");
   let [email, setEmail] = useState("");
   let [phone, setPhone] = useState("");
 
   let dispatch = useDispatch();
 
+  let nav = useNavigate();
+  
+  // let i=3;
+
   let handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(addUser({name:name,email:email,phone:phone}));
+    dispatch(addUser({id:users.length+1,name:name,email:email,phone:phone}));
+    nav('/home');
   };
 
   return (
     <div>
+      <div>
+        <h1>Add User</h1>
+      </div>
       <div className="container text-start">
         <div className="row justify-content-center">
           <div className="col-6">
