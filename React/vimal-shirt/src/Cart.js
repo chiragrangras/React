12a5bf2f8cb0bb591760069a35851dec95b4f3fd
Chirag Rangras <chiragrangras012@gmail.com>
2024-04-28 20:16:25 +0530

    modfied file

--- React/vimal-shirt/src/Cart.js
+++ React/vimal-shirt/src/Cart.js
@@ -2,9 +2,12 @@ import React, { useContext } from "react";
 import { CartDetails } from "./App";
 
 function Cart() {
-  const {cartDetail, setCartDetail} = useContext(CartDetails);
+  const { cartDetail, setCartDetail } = useContext(CartDetails);
   return (
-    <><div><h1>Cart</h1></div>
+    <>
+      <div>
+        <h1>Cart</h1>
+      </div>
       {cartDetail.map((product, index) => (
         <div key={index}>
           {`${product.name} - ${product.size} - Rs. ${product.price}`}
