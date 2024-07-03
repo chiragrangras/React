a030c1363e6dd8a6a024057c86d5cfbc4ae06a9f
Chirag Rangras <chiragrangras012@gmail.com>
2024-07-03 11:02:54 +0530

    new file

---
+++ React/psd-practice/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import "./nav.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="nav">
+          <a href="#">Logo</a>
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Courses</a>
+          <a href="#">Blog</a>
+          <button className="btn">Sign In</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
